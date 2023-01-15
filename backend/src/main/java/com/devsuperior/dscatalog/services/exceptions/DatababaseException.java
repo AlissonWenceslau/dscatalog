fa879edefff9e20206f7f020e4c20b377c7ed06f
Alisson Wenceslau <alisson.wenceslau@gmail.com>
2023-01-15 19:17:59 -0300

    DeleteById category

---
+++ backend/src/main/java/com/devsuperior/dscatalog/services/exceptions/DatababaseException.java
@@ -0,0 +1,10 @@
+package com.devsuperior.dscatalog.services.exceptions;
+
+public class DatababaseException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	public DatababaseException(String msg) {
+		super(msg);
+	}
+
+}
