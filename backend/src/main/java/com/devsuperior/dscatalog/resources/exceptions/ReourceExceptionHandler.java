713332502ffebcfbee1d22097e56d2492789f573
Alisson Wenceslau <alisson.wenceslau@gmail.com>
2023-05-20 16:27:56 -0300

    Exception handler bean validation

--- backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/ReourceExceptionHandler.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/ReourceExceptionHandler.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -38,6 +39,18 @@ public class ReourceExceptionHandler {
 		err.setMessage(e.getMessage());
 		err.setPath(request.getRequestURI());
 		
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> validation(MethodArgumentNotValidException e, HttpServletRequest request){
+		StandardError err = new StandardError();
+		HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("Validation Eception");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
 }
